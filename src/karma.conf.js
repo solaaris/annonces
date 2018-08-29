9dadeb92f9876023023757a9f63448fc226cb167
LASSERRE Kévin (EXT) <kevin.lasserre.ext@opt.nc>
2018-08-30 09:56:41 +1100

    fd

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-junit-reporter'),
-      require('pupeeteer')
+      require('puppeteer')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
